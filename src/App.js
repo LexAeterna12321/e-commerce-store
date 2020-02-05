81ee2e7317b60a552544cd7b6495cdf8ed774809
Lex Aeterna <lexaeterna12321@gmail.com>
2020-02-05 07:38:03 +0100

    added Shop,CollectionPreview,CollectionItem components plus styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import "./App.css";
-import HomePage from "./pages/HomePage/HomePage";
 import { Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import HomePage from "./pages/HomePage/HomePage";
+import ShopPage from "./pages/ShopPage/Shop";
+
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route path="/shop" exact component={ShopPage} />
       </Switch>
     </div>
   );
