7741ce43ef85760f34f9854f2232b6d9fd73ca52
Lex Aeterna <lexaeterna12321@gmail.com>
2020-02-05 20:31:40 +0100

    added templated navbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 import HomePage from "./pages/HomePage/HomePage";
 import ShopPage from "./pages/ShopPage/Shop";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/shop" exact component={ShopPage} />
