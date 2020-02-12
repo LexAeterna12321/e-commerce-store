1f9da7681e4096288a53ef7d02a24014e57731aa
Lex Aeterna <lexaeterna12321@gmail.com>
2020-02-12 14:14:41 +0100

    added CustomButton component in CollectionItem component to show possible user action / added appropriate styling

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./CollectionItem.scss";
-
+import CustomButton from "../CustomButton/CustomButton";
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   const image = `url(${imageUrl})`;
 
@@ -11,6 +11,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>ADD TO CART</CustomButton>
     </div>
   );
 };
