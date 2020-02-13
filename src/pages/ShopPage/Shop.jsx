f30702ee6a1b17f6fa16bf07c63968ce24b9b13c
Lex Aeterna <lexaeterna12321@gmail.com>
2020-02-13 21:58:42 +0100

    allocated shop data into its own reducer / added CollectionsOverview component

--- src/pages/ShopPage/Shop.jsx
+++ src/pages/ShopPage/Shop.jsx
@@ -1,24 +1,10 @@
-import React, { Component } from "react";
-import { SHOP_DATA } from "./ShopData";
-import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import React from "react";
+import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
