7f07c92a22c7cecf4ba24e8ccafe84aead3f91c4
Lex Aeterna <lexaeterna12321@gmail.com>
2020-02-13 21:23:07 +0100

    added localStorage data persist / created directoryReducer with reusable sections data

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
 
-export default combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
