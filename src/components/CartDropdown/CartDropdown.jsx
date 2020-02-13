f9f0c565621974edac1168134d4f54d3c59b1511
Lex Aeterna <lexaeterna12321@gmail.com>
2020-02-13 09:34:31 +0100

    added Reselect library to memoize cartItems value in state

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../CustomButton/CustomButton";
 import CartItem from "../CartItem/CartItem";
+import { selectCartItems } from "../../redux/cart/cartSelectors";
 import "./CartDropdown.scss";
 
-const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+const CartDropdown = ({ cartItems }) => {
+  console.log("cartDropdownFired");
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHEKCOUT</CustomButton>
     </div>
-    <CustomButton>GO TO CHEKCOUT</CustomButton>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./CartIcon.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartDropdown } from "../../redux/cart/cartActions";
-import { connect } from "react-redux";
-const CartIcon = ({ toggleCartDropdownVisibility }) => (
-  <div className="cart-icon" onClick={toggleCartDropdownVisibility}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
-  </div>
-);
+import { selectCartItemsCount } from "../../redux/cart/cartSelectors";
+
+const CartIcon = ({ toggleCartDropdownVisibility, itemCount }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCartDropdownVisibility}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartDropdownVisibility: isCartDropdownHidden =>
     dispatch(toggleCartDropdown(isCartDropdownHidden))
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
